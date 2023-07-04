0ad05e8005638e7b8af6406e831650edeedfe272
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2023-07-04 11:51:34 -0300

    Limpieza

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -45,42 +45,6 @@ db.Sequelize = Sequelize;
 sequelize.sync()
 // sequelize.sync({force: true})
 
+// operatorsAliases: false,
 
 module.exports = db;
-
-  // dialect: dbConfig.dialect,
-  // operatorsAliases: false,
-
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Connected...')
-//   })
-//   .catch(err => {
-//     console.log('Error' + err)
-//   })
-
-// const db = {}
-
-// db.Sequelize = Sequelize
-// db.sequelize = sequelize
-
-// db.paciente = require('./Paciente.js')(sequelize, DataTypes)
-// db.nombLesion = require('./NombLesion.js')(sequelize, DataTypes)
-// db.sexo = require('./Sexo.js')(sequelize, DataTypes)
-// db.estado = require('./Estado.js')(sequelize, DataTypes)
-// db.ubicacion = require('./Ubicacion.js')(sequelize, DataTypes)
-// db.lesion = require('./Lesion.js')(sequelize, DataTypes)
-// db.consulta = require('./Consulta.js')(sequelize, DataTypes)
-
-// Object.keys(db).forEach(key => {
-//   if (db[key] && db[key].associate) {
-//     db[key].associate(db);
-//   }
-// });
-
-// db.sequelize.sync({ force: false })
-//   .then(() => {
-//     console.log('Re-sincronización lista')
-//   })
-
-// module.exports = db
